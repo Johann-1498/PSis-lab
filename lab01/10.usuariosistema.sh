57097597a5275b630977efd90a963b6e71d6f1f8
dmolloc1 <139632142+dmolloc1@users.noreply.github.com>
2025-04-22 08:08:49 -0500

    Ejercicio 10 y el ejercicio del cuestionario

---
+++ lab01/10.usuariosistema.sh
@@ -0,0 +1,18 @@
+SS#!/bin/bash
+
+if [ $# -eq 1 ]
+then
+    echo "Buscando el nombre de usuario: $1"
+    #Se busca el nombre de usuario dentro del sistema 
+    user=$1
+    if grep "$user" /etc/passwd; then
+        echo "SI"
+    else
+        echo "NO"
+    fi
+else
+    echo "Error | Ingrese una sola palabra para buscar"
+fi
+
+
+
---
+++ lab01/11.borrar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+archivo=$1
+dir_temp="/tmp"
+ruta_archivo_temp="$dir_temp/$(basename "$archivo")"
+
+# Se realiza la copia
+cp "$archivo" "$dir_temp"
+
+# Se comprueba que la copia se realizo correctamente
+if [ -f "$ruta_archivo_temp" ]; then
+    echo "Archivo copiado correctamente en el directorio : $dir_temp"
+    echo "Eliminando archivo"
+    rm "$archivo"
+else
+    echo "Error| Archivo no copiado"
+fi
