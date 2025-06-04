df1abfc0659e7d7404e3e6fcf64fc141c617e713
Johann-1498 <jcaceresruiz@unsa.edu.pe>
2025-06-04 08:31:06 -0500

    Odd even sort code documentado

--- lab06/odd.cpp
+++ lab06/odd.cpp
@@ -26,13 +26,28 @@ void Read_list(int a[], int n);
 void Odd_even_sort(int a[], int n);
 
 /*-----------------------------------------------------------------*/
+/* Function:  main
+ * Purpose:   Get command line arguments, generate or read list,
+ *            sort the list, and print the sorted list.
+ * In args:   argc, argv
+ * Out args:  none
+ */   
+
+
 int main(int argc, char* argv[]) {
+   /* Declare variables */
    int  n;
    char g_i;
    int* a;
 
+   /* Get command line arguments */
    Get_args(argc, argv, &n, &g_i);
    a = (int*) malloc(n*sizeof(int));
+   /* Generate or read list based on command line argument
+    * 'g' for generate, 'i' for input.
+    * If 'g', generate a random list of integers.
+    * If 'i', read integers from standard input.
+   */
    if (g_i == 'g') {
       Generate_list(a, n);
       Print_list(a, n, "Before sort");
@@ -50,8 +65,14 @@ int main(int argc, char* argv[]) {
 /*-----------------------------------------------------------------
  * Function:  Usage
  * Purpose:   Summary of how to run program
+ * In args:   prog_name
+ * Out args:  none
  */
 void Usage(const char* prog_name) {
+   /* Print usage message
+
+    */
+
    fprintf(stderr, "usage:   %s <n> <g|i>\n", prog_name);
    fprintf(stderr, "   n:   number of elements in list\n");
    fprintf(stderr, "  'g':  generate list using a random number generator\n");
@@ -64,13 +85,16 @@ void Usage(const char* prog_name) {
  * Out args:  n_p, g_i_p
  */
 void Get_args(int argc, char* argv[], int* n_p, char* g_i_p) {
+   /* Check command line arguments */
    if (argc != 3 ) {
       Usage(argv[0]);
       exit(0);
    }
+   /* Check that n is a positive integer and g_i is 'g' or 'i' */
    *n_p = atoi(argv[1]);
    *g_i_p = argv[2][0];
 
+   /* Validate n and g_i */
    if (*n_p <= 0 || (*g_i_p != 'g' && *g_i_p != 'i') ) {
       Usage(argv[0]);
       exit(0);
@@ -85,8 +109,9 @@ void Get_args(int argc, char* argv[], int* n_p, char* g_i_p) {
  * Out args:  a
  */
 void Generate_list(int a[], int n) {
+   
+   /* Generate a list of n random integers in the range 0 to RMAX-1 */
    int i;
-
    srand(0);
    for (i = 0; i < n; i++)
       a[i] = rand() % RMAX;
@@ -98,8 +123,9 @@ void Generate_list(int a[], int n) {
  * In args:   a, n
  */
 void Print_list(int a[], int n, const char* title) {
+   
+   /* Print the elements of the list with a title */
    int i;
-
    printf("%s:\n", title);
    for (i = 0; i < n; i++)
       printf("%d ", a[i]);
@@ -114,8 +140,9 @@ void Print_list(int a[], int n, const char* title) {
  * Out args:  a
  */
 void Read_list(int a[], int n) {
+   
+   /* Read n integers from standard input into the array a */
    int i;
-
    printf("Please enter the elements of the list\n");
    for (i = 0; i < n; i++)
       scanf("%d", &a[i]);
@@ -130,6 +157,14 @@ void Read_list(int a[], int n) {
 void Odd_even_sort(
       int  a[]  /* in/out */, 
       int  n    /* in     */) {
+   /*
+   
+      * Sort the list a using odd-even transposition sort.
+      * The algorithm works by repeatedly performing two phases:
+      * 1. In the even phase, compare and swap adjacent elements at even indices.
+      * 2. In the odd phase, compare and swap adjacent elements at odd indices.
+      * This process is repeated until the list is sorted.
+   */
    int phase, i, temp;
 
    for (phase = 0; phase < n; phase++) 
