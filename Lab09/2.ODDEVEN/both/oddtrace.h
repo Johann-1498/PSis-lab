4d5466fdeb2019995934633ba83ab44e996b86d4
DANIEL <118449221+Dan1elMon@users.noreply.github.com>
2025-07-15 19:19:01 -0500

    Update oddtrace.h

--- Lab09/2.ODDEVEN/both/oddtrace.h
+++ Lab09/2.ODDEVEN/both/oddtrace.h
@@ -14,13 +14,13 @@
 
 const int RMAX = 100;
 
-// Prototipos
+// Prototipos (sin main())
 void Usage(const char* prog_name);
 void Get_args(int argc, char* argv[], int* n_p, char* g_i_p, int* num_procs, int* num_threads);
 void Generate_list(int a[], int n);
 void Print_list(int a[], int n, const char* title);
 void Read_list(int a[], int n);
-void Odd_even_sort(int a[], int n);
+void Odd_even_sort(int a[], int n, int num_procs, int num_threads);
 void Phase_process(int* arr, int n, int phase, int num_threads);
 
 #endif
